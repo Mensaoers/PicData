df8277c31fd4d7c30c172614ab6948e5b0405701
pengpeng <pengpeng@trackview.co>
2022-01-22 15:16:21 +0800

    优化图集列表页面排版

--- PicData/PicData/Index/ContentList/View/PicContentView.m
+++ PicData/PicData/Index/ContentList/View/PicContentView.m
@@ -21,13 +21,24 @@ static CGFloat sideMargin = 5;
 + (CGFloat)itemWidth:(CGFloat)wholeWidth {
     // cell的排布, 想让cell的宽度逐渐增大, 大到一定程度, 加一个cell, 以此往复
     CGFloat sugWidth = 150;
+
+    if (wholeWidth < 400) {
+        sugWidth = 100; // 3
+    } else if (wholeWidth < 650) {
+        sugWidth = 150; // 4
+    } else if (wholeWidth < 1000) {
+        sugWidth = 180; // 5
+    } else {
+        sugWidth = 220;
+    }
+
 #if TARGET_OS_MACCATALYST
     sugWidth = 180;
 #endif
     // 这一行至少可以放几个
     NSInteger count = floorf(wholeWidth / sugWidth);
-    CGFloat itemWidth = wholeWidth / count;
-    return itemWidth - 2 * sideMargin;
+    CGFloat itemWidth = (wholeWidth - (count - 1) * 2 * sideMargin) / count;
+    return itemWidth;
 }
 + (CGFloat)itemHeight:(CGFloat)wholeWidth {
     CGFloat itemHeight = [PicContentView itemWidth:wholeWidth] * PicContentCellSCALE + 50;
@@ -43,8 +54,8 @@ static CGFloat sideMargin = 5;
 + (instancetype)collectionView:(CGFloat)wholeWidth {
 
     PicContentViewFlowLayout *layout = [[PicContentViewFlowLayout alloc] init];
-    layout.itemSize = [PicContentView itemSize:wholeWidth];
-    layout.sectionInset = UIEdgeInsetsMake(2 * sideMargin, sideMargin, 2 * sideMargin, sideMargin);
+    layout.itemSize = [PicContentView itemSize:wholeWidth - 4 * sideMargin];
+    layout.sectionInset = UIEdgeInsetsMake(2 * sideMargin, 2 * sideMargin, 2 * sideMargin, 2 * sideMargin);
     layout.minimumLineSpacing = 2 * sideMargin;
     layout.minimumInteritemSpacing = 2 * sideMargin;
     layout.scrollDirection = UICollectionViewScrollDirectionVertical;
@@ -59,7 +70,7 @@ static CGFloat sideMargin = 5;
 
     // mac端拖拽之后, 界面重新适配
     PicContentViewFlowLayout *layout = (PicContentViewFlowLayout *)self.collectionViewLayout;
-    layout.itemSize = [PicContentView itemSize:self.mj_w];
+    layout.itemSize = [PicContentView itemSize:self.mj_w - 4 * sideMargin];
 }
 
 @end

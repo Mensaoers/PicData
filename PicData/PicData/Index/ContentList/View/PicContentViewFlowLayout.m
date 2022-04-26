fbe64c8b337dc0291505243c3ea284cf80f18260
pengpeng <pengpeng@trackview.co>
2022-04-26 14:57:19 +0800

    代码优化

--- PicData/PicData/Index/ContentList/View/PicContentViewFlowLayout.m
+++ PicData/PicData/Index/ContentList/View/PicContentViewFlowLayout.m
@@ -10,28 +10,28 @@
 
 @implementation PicContentViewFlowLayout
 
-- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
-    /// 暂且设为NO, 不让他改
-    return NO;
-}
-
-- (NSArray<__kindof UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
-    NSArray *arr = [self getCopyOfAttributes:[super layoutAttributesForElementsInRect:rect]];
-    for (UICollectionViewLayoutAttributes *attribute in arr) {
-        // 在这里修改每个item的属性, 包括frame, size之类的
-        CGSize size = attribute.size;
-        size.width = self.itemSize.width;
-        attribute.size = size;
-    }
-    return arr;
-}
-
-- (NSArray *)getCopyOfAttributes:(NSArray *)attributes {
-    NSMutableArray *copyArr = [NSMutableArray new];
-    for (UICollectionViewLayoutAttributes *attribute in attributes) {
-        [copyArr addObject:[attribute copy]];
-    }
-    return copyArr;
-}
+//- (BOOL)shouldInvalidateLayoutForBoundsChange:(CGRect)newBounds {
+//    /// 暂且设为NO, 不让他改
+//    return NO;
+//}
+//
+//- (NSArray<__kindof UICollectionViewLayoutAttributes *> *)layoutAttributesForElementsInRect:(CGRect)rect {
+//    NSArray *arr = [self getCopyOfAttributes:[super layoutAttributesForElementsInRect:rect]];
+//    for (UICollectionViewLayoutAttributes *attribute in arr) {
+//        // 在这里修改每个item的属性, 包括frame, size之类的
+//        CGSize size = attribute.size;
+//        size.width = self.itemSize.width;
+//        attribute.size = size;
+//    }
+//    return arr;
+//}
+//
+//- (NSArray *)getCopyOfAttributes:(NSArray *)attributes {
+//    NSMutableArray *copyArr = [NSMutableArray new];
+//    for (UICollectionViewLayoutAttributes *attribute in attributes) {
+//        [copyArr addObject:[attribute copy]];
+//    }
+//    return copyArr;
+//}
 
 @end
